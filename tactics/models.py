bcd9a39dad0989af420cba47a6bfe8bf5594cf74
NAMAN MISHRA <namanmishraec1045@gmail.com>
2023-05-19 11:36:28 +0530

    Added tactic model with description

--- tactics/models.py
+++ tactics/models.py
@@ -3,8 +3,9 @@ from django_quill.fields import QuillField
 
 # Create your models here.
 class Tactic(models.Model):
-    tactic_id = models.CharField(verbose_name="Tactic ID", unique=True, null=False, blank=False, max_length=10)
-    tactic_name = models.TextField(verbose_name="Tactic Name", null=False, blank=False, unique=False)
+    tactic_id = models.CharField(verbose_name="ID", unique=True, null=False, blank=False, max_length=10)
+    tactic_name = models.TextField(verbose_name="Name", null=False, blank=False, unique=False)
+    tactic_description = QuillField()
 
     def __str__(self):
         return str(self.tactic_id) + " :: " + str(self.tactic_name)
