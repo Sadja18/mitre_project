43146ad1c39c7f5d330b3219d40ddda251a600a7
NAMAN MISHRA <namanmishraec1045@gmail.com>
2023-05-19 11:55:02 +0530

    in adversary preference: the option selection is limited

--- adversary_preferences/models.py
+++ adversary_preferences/models.py
@@ -8,9 +8,10 @@ from techniques.models import Technique
 # Create your models here.
 class AdversaryGroupPreference(models.Model):
     adversary_group = models.ForeignKey(AdversaryGroup, on_delete=models.CASCADE)
-    tactic = models.ForeignKey(Tactic, on_delete=models.CASCADE)
-    techniques = models.ManyToManyField(Technique)
-    subtechniques = models.ManyToManyField(SubTechnique, blank=True)
+    tactics = models.ForeignKey(Tactic, on_delete=models.CASCADE)
+    techniques = models.ManyToManyField(Technique, limit_choices_to={'tactics__in': [tactics]})
+    subtechniques = models.ManyToManyField(SubTechnique, blank=True, limit_choices_to={'technique__in': techniques})
 
     def __str__(self):
         return f"{self.adversary_group.group_name} - {self.tactic.tactic_name}"
+
