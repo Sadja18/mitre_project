51d92ebff2cae356ac818cce6c8e5f9cd0bad6ab
NAMAN MISHRA <namanmishraec1045@gmail.com>
2023-05-22 12:17:23 +0530

    Version: May 22: 1217: - Added Missing Techniques - Corrected Names of some techniques - Now, admin can search techniques based on tactics name and tactics id

--- sub_techniques/models.py
+++ sub_techniques/models.py
@@ -4,7 +4,7 @@ from techniques.models import Technique
 
 # Create your models here.
 class SubTechnique(models.Model):
-    subtechnique_id = models.CharField(max_length=50, unique=True, verbose_name="ID")
+    subtechnique_id = models.CharField(max_length=50, unique=False, verbose_name="ID")
     subtechnique_name = models.CharField(max_length=100, verbose_name="Name")
     technique = models.ForeignKey(Technique, on_delete=models.CASCADE)
 
--- techniques/admin.py
+++ techniques/admin.py
@@ -1,3 +1,4 @@
+from typing import Any, Tuple
 from django.contrib import admin
 
 from django.contrib.admin.widgets import FilteredSelectMultiple
@@ -8,7 +9,7 @@ from .models import Technique
 # admin.site.register(Technique)
 class TechniqueAdmin(admin.ModelAdmin):
     list_display = ('technique_id', 'technique_name', 'get_tactics')
-    search_fields = ['technique_name', 'technique_id']
+    search_fields = ['technique_name', 'technique_id', 'tactics__tactic_name', 'tactics__tactic_id']
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == 'tactics':
